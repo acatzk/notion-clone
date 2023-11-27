095576c13ea68f2e8d7186611604dd65e469485b
acatzk <joshuaimalay@gmail.com>
2023-11-27 15:43:46 +0800

    feat: create sidebar functionality

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+type Props = Record<string, unknown>
+
+const DocumentsPage: FC<Props> = (): JSX.Element => {
+  return <div>DocumentsPage</div>
+}
+
+export default DocumentsPage
